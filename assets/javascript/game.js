eed51b9cf9039d23a7a82197c31ebbdb3de5cc75
Mehathab4eva <mehathab4eva@gmail.com>
2017-10-05 20:38:58 -0600

    corrected the bugs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var userName = prompt("Please enter your name?");
 alert("Welcome! "+userName+". Let's play the guessing game.");
 
 
-	var alphabet = ["a", "b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+	var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 	var userWon = 0;
 	var userLost = 0;
 	var userTries = 11;
@@ -15,46 +15,56 @@ alert("Welcome! "+userName+". Let's play the guessing game.");
 	uTries.innerHTML = "Your tries left: "+userTries+ " time/s";
 
 
-	var random = Math.floor(Math.random() *26);
+	var random = Math.floor(Math.random() * 26);
 
 	var computerChoice = alphabet[random];
+	console.log(computerChoice);
 	// 
 
 	// var x = event.which || event.keyCode;
 	// var userChoice = String.fromCharCode(x);
 	
-		document.onkeyup = function(event) {
+	document.onkeyup = function(event) {
      var userChoice = event.key;
 	
 
-	if ((userTries> 0)&&(computerChoice === userChoice)) {
-		userTries = 11;
+	if ((userTries > 0)&&(computerChoice === userChoice)) {
 		userWon++;
 		uWins.innerHTML = "You won: "+userWon+ " time/s";
 		document.getElementById("newDiv").innerHTML= "YOU WON!. Lets give it another shot.";
-		uTries.innerHTML = "Your tries left: "+userTries+ " time/s";
     	// body.css("background-color: green;");
     	alert("Computer choose -"+computerChoice+". Good Job."); 
+    	reset()
 
 	}
 
-	else if ((userTries> 0)&&(computerChoice !== userChoice)) {
+	else if ((userTries >= 0) && (computerChoice !== userChoice)) {
 		userTries--;
 		document.getElementById("newDiv").innerHTML= "No match. Try again harder.";
     	uTries.innerHTML = "Your tries left: "+userTries+ " time/s";
-	}
+  //   	if (userTries = 0)
+	 // 	{
+		// userLost++;
+		// userTries = 11;
+		// document.getElementById("newDiv").innerHTML= "You have lost. Let's do it again.";
+  //   	uLosses.innerHTML = "You  Lost: "+userLost + " time/s";
+		// return();
+		// }
+		 if (userTries == 0){
+				userLost++;
+				document.getElementById("newDiv").innerHTML= "You have lost. Let's do it again.";
+		    	uLosses.innerHTML = "You  Lost: "+ userLost + " time/s";
+		    	reset()
+			}
 
-	else if (userTries <= 0) {
-		userLost++;
-		userTries = 11;
-		document.getElementById("newDiv").innerHTML= "You have lost. Let's do it again.";
-    	uLosses.innerHTML = "You  Lost: "+userLost + " time/s";
 	}
 
 }
 
-
-	// console.log(uWins);
-
-
-
+function reset(){
+	random = Math.floor(Math.random() * 26);
+    computerChoice = alphabet[random];
+    userTries = 11;
+    uTries.innerHTML = "Your tries left: "+userTries+ " time/s";
+    console.log(computerChoice);
+}
